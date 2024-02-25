ef2920cad898fa4eeeb0beb216ee42e7995bb7d3
merin017 <merin8141@gmail.com>
2024-02-25 22:08:49 +0200

    almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,57 @@ import { useState } from 'react';
 import Log from './components/Log.jsx';
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+const initialGameBoard = [
+  [null,null,null ],
+  [null,null,null],
+  [null,null,null]
+];
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer='X';
+
+  if( gameTurns.length>0 && gameTurns[0].player==='X' ){
+ currentPlayer='O';
+}
+ return currentPlayer;
+}
+
 function App() {
-  const [gameTurns,setGameTurns] = useState([])
-  const [activePlayer,setActivePlayer] = useState('X'); 
-  function handleSelectSquare (rowIndex,colIndex){
-    setActivePlayer((curActivePlayer)=>curActivePlayer === 'X' ? 'O' : 'X');
+  const [gameTurns,setGameTurns] = useState([]);
+  //const [hasWinner,setHasWinner] = useState(false);
+  // const [activePlayer,setActivePlayer] = useState('X'); 
+ const activePlayer = deriveActivePlayer(gameTurns);
+ 
+ let gameBoard=initialGameBoard;
+ for(const turn of gameTurns){
+    const{ square , player }=turn ; 
+    const{row,col}=square;
+
+    gameBoard[row][col]=player;
+ }
+ let winner;
+ 
+ for(const combination of WINNING_COMBINATIONS){
+  const firstSquareSymbol= gameBoard[combination[0].row][combination[0].column];
+  const secondSquareSymbol=gameBoard[combination[1].row][combination[1].column];
+  const thirdSquareSymbol=gameBoard[combination[2].row][combination[2].column];
+ if(
+  firstSquareSymbol && 
+  firstSquareSymbol===secondSquareSymbol && 
+  firstSquareSymbol===thirdSquareSymbol
+  ) {
+    winner=firstSquareSymbol;
+  }
+
+}
+
+ 
+ function handleSelectSquare (rowIndex,colIndex){
+   // setActivePlayer((curActivePlayer)=>curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns((prevTurns)=>{
-     let currentPlayer='X';
-     if( prevTurns.length>0 && prevTurns[0].player==='X' ){
-      currentPlayer='O';
-     }
+     const currentPlayer= deriveActivePlayer(prevTurns);
 
 
       const updatedTurns = [{square:{ row:rowIndex,col:colIndex },player:currentPlayer },
@@ -28,12 +69,14 @@ function App() {
       <Player initialName='Player 1' symbol='X' isActive={activePlayer==='X'}/>
       <Player initialName='Player 2' symbol='O' isActive={activePlayer==='O'}/>
     </ol>
-    <GameBoard onSelectSquare={handleSelectSquare}
-               turns={gameTurns}
-    />
+    {winner && <p>You won,{winner}!</p>}
+    <GameBoard 
+    onSelectSquare={handleSelectSquare}
+    board={gameBoard}
+   />
    
     </div>
-    <Log/>
+    <Log turns={gameTurns} />
    </main>
 );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,6 @@
-
-const initialGameBoard = [
-    [null,null,null ],
-    [null,null,null],
-    [null,null,null]
-];
-export default function GameBoard ({onSelectSquare,turns}) {
- let gameBoard=initialGameBoard;
- for(const turn of turns){
-    const{ square , player }=turn ; 
-    const{row,col}=square;
-
-    gameBoard[row][col]=player;
- }
-
-
-
-
-  // const[gameBoard,setGameBoard] = useState(initialGameBoard);
+export default function GameBoard ({onSelectSquare,board}) {
+ 
+ // const[gameBoard,setGameBoard] = useState(initialGameBoard);
 //function handleSelectSquare(rowIndex,colIndex){
   //  setGameBoard((prevGameBoard)=>{
     // const updatedBoard = [...prevGameBoard.map(innerArray=> [...innerArray])];
@@ -28,12 +12,14 @@ export default function GameBoard ({onSelectSquare,turns}) {
 //}
 
     return  <ol id='game-board'>
-        {gameBoard.map((row,rowIndex)=>(
+        {board.map((row,rowIndex)=>(
         <li key={rowIndex} >
        <ol>
            {row.map((playerSymbol,colIndex)=>(
            <li key={colIndex}>
-            <button onClick={()=>onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button> 
+            <button onClick={()=>onSelectSquare(rowIndex,colIndex)} disabled={playerSymbol!==null}>
+              {playerSymbol}
+            </button> 
             </li>
             ))}
 
